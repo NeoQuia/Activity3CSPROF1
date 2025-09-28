258573417905e0d76fe6b2b3b3679c351c585465
NeoQuia <quianeomarnel_bscs@plmun.edu.ph>
2025-09-29 03:31:07 +0800

    Add files via upload

---
+++ includes/functions.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+
+// Utility functions
+function sanitize_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function validate_email($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function validate_password($password) {
+    // Password must be at least 6 characters
+    return strlen($password) >= 6;
+}
+
+function is_logged_in() {
+    return isset($_SESSION['user_id']);
+}
+
+function is_admin() {
+    return isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'admin';
+}
+
+function redirect($url) {
+    header("Location: $url");
+    exit();
+}
+
+function generate_csrf_token() {
+    if (!isset($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+
+function verify_csrf_token($token) {
+    return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+}
+
+function json_response($data, $status = 200) {
+    http_response_code($status);
+    header('Content-Type: application/json');
+    echo json_encode($data);
+    exit();
+}
+?>
